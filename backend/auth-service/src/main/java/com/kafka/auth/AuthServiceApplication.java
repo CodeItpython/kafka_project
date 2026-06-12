6e818f441330d070fb7969faf55b25f1741bfe64
gunwoo08 <99863853+CodeItpython@users.noreply.github.com>
2026-06-12 12:48:24 +0900

    Repository 스캔 경고 정리

--- backend/auth-service/src/main/java/com/kafka/auth/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/com/kafka/auth/AuthServiceApplication.java
@@ -1,11 +1,12 @@
 package com.kafka.auth;
 
+import org.springframework.boot.autoconfigure.data.elasticsearch.ReactiveElasticsearchRepositoriesAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = ReactiveElasticsearchRepositoriesAutoConfiguration.class)
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
